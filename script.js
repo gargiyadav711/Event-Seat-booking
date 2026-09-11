53ccccca341999c088311cd6e47c475ad2921139
Gargi Yadav <gargiyadav711@gmail.com>
2026-09-11 21:31:44 +0530

    seat selection

--- script.js
+++ script.js
@@ -8,4 +8,15 @@ signupBtn.addEventListener("click", () => {
 function closeSignup() {
     signupPopup.classList.remove("active");
 }
-
+const seatLayout = document.getElementById("seatLayout");
+const selectedSeatsText = document.getElementById("selectedSeats");
+const totalPriceText = document.getElementById("totalPrice");
+const confirmBooking = document.getElementById("confirmBooking");
+const seatSection = document.getElementById("seatSection");
+const seatPrice = 250;
+const totalSeats = 60;
+const bookedSeats = [3, 8, 15, 22, 35];
+const STORAGE_KEY = "selectedSeats";
+function getSelectedSeats() { 
+    return JSON.parse(localStorage.getItem(STORAGE_KEY)) || []; 
+}
